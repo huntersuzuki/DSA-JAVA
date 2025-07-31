a8524001fe95d4c0bcbcbf85c41bd8f3d7db3da5
prana <pranaytb777@gmail.com>
2025-07-31 17:09:58 +0530

    Enhance MyInterfaceImpl to demonstrate multiple lambda implementations and their outputs

--- src/com/functionalProgramming/java8/examples/functionalInterface/lambraExpression/MyInterfaceImpl.java
+++ src/com/functionalProgramming/java8/examples/functionalInterface/lambraExpression/MyInterfaceImpl.java
@@ -3,7 +3,15 @@ package com.functionalProgramming.java8.examples.functionalInterface.lambraExpre
 public class MyInterfaceImpl{
     public static void main(String[] args) {
         MyInterface m = () -> "Hello from lambda MyInterface";
-        System.out.println(m.sayHello());;
+        System.out.println(m.sayHello());
+        // Because of lambda expression we don't have to create a separate implementation class
+        // And we can implement the same abstract method multiple times.
+        // Example -
+        MyInterface mi = () -> "Hello guys how are you";
+        System.out.println(mi.sayHello());
+        // Output -
+        // Hello from lambda MyInterface
+        // Hello guys how are you
     }
 
 }
