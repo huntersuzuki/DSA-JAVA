6e495bb2ae07c95b22d736d2f7c41b836be40fb5
prana <pranaytb777@gmail.com>
2026-03-16 07:52:22 +0000

    Add LinkedList usage examples, custom `ManualLinkedList` implementation, update .gitignore, and IntelliJ project configuration files

---
+++ src/com/collectionFramework/Linked_List/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(11);
+        linkedList.add(12);
+        linkedList.add(13);
+        linkedList.get(2);
+        linkedList.addFirst(2); // Ads an element at the first position
+        linkedList.addLast(55); // Ads an element at the last position
+        linkedList.getFirst(); // Gets an element at the first position
+        linkedList.getLast(); // Gets an element at the last position
+        System.out.println(linkedList);
+//        linkedList.remove(12);
+        linkedList.removeIf(x -> x%2 == 0); //  Removes nodes based on a condition.
+        System.out.println(linkedList);
+
+        LinkedList<String> animals = new LinkedList<>(Arrays.asList("Cat","Dog","Elephant"));
+        LinkedList<String> animalsToRemove = new LinkedList<>(Arrays.asList("Dog","Lion"));
+        animals.removeAll(animalsToRemove);
+        System.out.println(animals);
+    }
+}
---
+++ src/com/collectionFramework/Linked_List/src/ManualLinkedList.java
@@ -0,0 +1,22 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class ManualLinkedList {
+    public static void main(String[] args) {
+        Node head = new Node(20);
+        Node second = new Node(222);
+        Node third = new Node(898);
+
+        // Linking the nodes to each other
+        head.next = second;
+        second.next = third;
+        // Now head is the start of your list: 20 -> 222 -> 898 -> null
+    }
+}
