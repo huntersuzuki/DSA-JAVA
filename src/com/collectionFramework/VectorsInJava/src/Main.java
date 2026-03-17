20210a3b7ee810a6114f17803ae3a915bb250e7e
prana <pranaytb777@gmail.com>
2026-03-17 07:50:30 +0000

    Add IntelliJ IDEA config files, `.gitignore`, initial project structure, and `Main` class with thread-safe `Vector` example

---
+++ src/com/collectionFramework/VectorsInJava/src/Main.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // The default capacity of a vector is 10.
+        // We can give an initial capacity value into its constructor.
+//        Vector<Integer> vector = new Vector<>(5,3);
+//        vector.add(1);
+//        vector.add(1);
+//        vector.add(1);
+//        vector.add(1);
+//        vector.add(1);
+//        System.out.println(vector.capacity());
+//        vector.add(1);
+//        System.out.println(vector.capacity());
+//
+//        LinkedList<Integer> linkedList = new LinkedList<>();
+//        linkedList.add(22);
+//        linkedList.add(89);
+//        linkedList.add(69);
+//        linkedList.add(67);
+//        Vector<Integer> vector1 = new Vector<>(linkedList);
+//        System.out.println(vector1);
+
+
+//        ArrayList<Integer> list = new ArrayList<>();
+        Vector<Integer> list = new Vector<>();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                list.add(i);
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                list.add(i);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Size of list: "+ list.size());
+    }
+}
